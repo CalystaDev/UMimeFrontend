9487da3d4aa4b9b4374d8ae5d019723b098bc894
Ria Patel <ria2004.patel@gmail.com>
2024-08-24 12:19:58 -0400

    select host dropdown feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,20 +4,35 @@ import { MimeCardComponent } from '../mime-card/mime-card.component';
 import { MimeNewComponent } from '../mime-new/mime-new.component';
 import { MatIconModule } from '@angular/material/icon';
 import { Mime, PastMimesService } from '../../services/past-mimes.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MimeCardComponent, MimeNewComponent, MatIconModule],
+  imports: [CommonModule, MimeCardComponent, MimeNewComponent, MatIconModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit { 
   mimes: Mime[] = [];
+  mimePrompt: string = ' '
+  selectedHost: string = ''
+
+  hosts: string[] = ['Host 1', 'Host 2', 'Host 3', 'Host 4'];
 
   constructor(private mimeService: PastMimesService) {}
 
   ngOnInit() {
     this.mimes = this.mimeService.getMimes();
+  }
+
+  onLetsMimeClick() {
+    console.log('Selected Host:', this.selectedHost);
+    
+    if (this.selectedHost) {
+      alert(`Let's mime with ${this.selectedHost}!`);
+    } else {
+      alert('Please select a host before proceeding.');
+    }
   }
 }
