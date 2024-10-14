e48dc18e6ef33f6d3e0d14485c8d2f94ba3eee62
TheFunnyPapa <gabrielragy@gmail.com>
2024-10-14 16:27:51 -0400

    Fix: Post-merge mods and fixes

--- src/app/screens/host-directory/host-directory.component.ts
+++ src/app/screens/host-directory/host-directory.component.ts
@@ -32,39 +32,4 @@ export class HostDirectoryComponent implements OnInit{
     console.log('Home clicked');
     this.router.navigate(['/']);
   }
-
-  // hosts = [
-  //   {
-  //     hostPhotoURL: 'assets/mua_test.png',
-  //     hostDisplayName: 'MUA #1',
-  //     uses: 37000,
-  //     tags: ['soft', 'cali girl vibes']
-  //   },
-  //   {
-  //     hostPhotoURL: 'assets/sports.png',
-  //     hostDisplayName: 'Sports #1',
-  //     uses: 10000,
-  //     tags: ['deep', 'fatherly', 'italian accent']
-  //   },
-  //   {
-  //     hostPhotoURL: 'assets/rapper.png',
-  //     hostDisplayName: 'Rapper #1',
-  //     uses: 75000,
-  //     tags: ['mumbler', 'side commentaries']
-  //   },
-  //   {
-  //     hostPhotoURL: 'assets/chef.png',
-  //     hostDisplayName: 'Master Chef #1',
-  //     uses: 62000,
-  //     tags: ['coach vibes', 'scary', 'judgy']
-  //   },
-  //   {
-  //     hostPhotoURL: 'assets/teacher.png',
-  //     hostDisplayName: 'Teacher #1',
-  //     uses: 50000,
-  //     tags: ['strict', 'wise', 'loves the kids']
-  //   }
-  // ];
-
-
 }
